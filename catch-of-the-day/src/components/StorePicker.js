7e75f2500070a5c6cca2907dd212c257fc202708
Mark O'Beirne <markobeirne00@gmail.com>
2021-03-01 10:43:33 +0000

    Add event, ref and binding
    - Add onSubmit event
    - Add ref for input field
    - Declare property of goToStore that's equal to an arrow function in
    order to set proper binding

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore = (event) => {
+        // 1. Prevent form from submitting
+        event.preventDefault();
+        // 2. Get text from that input
+        console.log(this);
+        // 3. Change page to /store/store-name
+
+    };
     render() {
         return (
-            <form action="" className="store-selector">
+            <form action="" className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" />
+                <input
+                    type="text"
+                    required
+                    placeholder="Store Name"
+                    defaultValue={getFunName()}
+                    ref={this.myInput}
+                />
                 <button type="submit">Visit Store!</button>
             </form>
         )
