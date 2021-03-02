9fd3da5ee60d84db5d1049df6d2003dfd3b7415a
Mark O'Beirne <markobeirne00@gmail.com>
2021-03-02 18:14:35 +0000

    Add initial state and method to update state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,21 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    };
+    addFish = (fish) => {
+        // 1 - Take a copy of the existing state
+        const fishes = { ...this.state.fishes };
+        // 2 - Add new fish to that fishes variable
+        fishes[`fish${Date.now()}`] = fish; // Date.now() provides you with a unique identifier (provided you're not adding more than one fish a millisecond)
+        // 3 Set the new fishes object to state
+        this.setState({
+            fishes: fishes
+        })
+    }
+    
     render() {
         return (
             <div className="catch-of-the-day">
@@ -11,7 +26,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Daily" />
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} />
             </div>
         );
     }
